e3ad3ee1e4a71e20be862fb9f226f0f7cf534626
Jeff Morton <jmorton138@gmail.com>
2022-12-28 09:42:35 +0000

    [ADD] Connects to mysql

--- EmployeeManagerApp/src/App.java
+++ EmployeeManagerApp/src/App.java
@@ -1,5 +1,25 @@
+import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // gets the required driver
+        Class.forName("com.mysql.jdbc.Driver");
+        // creates connection with sql
+        String dbName = "ems";
+        String db_username = "root";
+        String db_password = "password";
+        Connection con = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/" + dbName, db_username, db_password);
+        System.out.println(con);
+        LoginPage loginPage = new LoginPage(con);
+        loginPage.setVisible(true);
     }
 }
